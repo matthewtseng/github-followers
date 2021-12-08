feb3a104aa3d9e18843d9c9426e1448216c0c1b3
Matthew Tseng <matthewwktseng@gmail.com>
2021-12-07 18:01:41 -0800

    search functionality implemented

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -18,14 +18,16 @@ class FollowerListVC: UIViewController {
     var page: Int = 1
     var hasMoreFollowers = true
     var followers: [Follower] = []
+    var filteredFollowers: [Follower] = []
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureCollectionView()
         configureViewController()
+        configureSearchController()
+        configureCollectionView()
         getFollowers(username: username, page: page)
         configureDataSource()
     }
@@ -47,6 +49,16 @@ class FollowerListVC: UIViewController {
         collectionView.delegate = self
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseIdentifier)
+    }
+    
+    private func configureSearchController() {
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self // Delegate for search results
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Search for a username"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
     }
     
     func getFollowers(username: String, page: Int) {
@@ -76,7 +88,7 @@ class FollowerListVC: UIViewController {
                         return
                     }
                     
-                    self.updateData()
+                    self.updateData(on: self.followers)
                 case .failure(let error):
                     self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Okay")
             }
@@ -93,7 +105,7 @@ class FollowerListVC: UIViewController {
     }
     
     // Snapshot data function
-    private func updateData() {
+    private func updateData(on followers: [Follower]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
@@ -123,5 +135,21 @@ extension FollowerListVC: UICollectionViewDelegate {
             page += 1
             getFollowers(username: username, page: page)
         }
+    }
+}
+
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        // Create filter from the text in search bar, ensure it isn't empty
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        
+        // Filter the followers based on filter above
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
+        // Update CollectionView to show filtered followers
+        updateData(on: filteredFollowers)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        updateData(on: followers)
     }
 }
